e6a8f191efe3acee7350157603a11c516138bae2
TakeshiMusgrave <tkm45@cornell.edu>
2019-12-02 10:51:09 -0500

    Added semihard and hard options to TripletMarginMiner

--- pytorch_metric_learning/miners/triplet_margin_miner.py
+++ pytorch_metric_learning/miners/triplet_margin_miner.py
@@ -8,14 +8,21 @@ import torch
 class TripletMarginMiner(BasePostGradientMiner):
     """
     Returns triplets that violate the margin
+    Args:
+    	margin
+    	type_of_triplets: options are "all", "hard", or "semihard".
+    		"all" means all triplets that violate the margin
+    		"hard" is a subset of "all", but the negative is closer to the anchor than the positive
+    		"semihard" is a subset of "all", but the negative is further from the anchor than the positive
     """
-    def __init__(self, margin, **kwargs):
+    def __init__(self, margin, type_of_triplets="all", **kwargs):
         super().__init__(**kwargs)
         self.margin = margin
         self.pos_pair_dist = 0
         self.neg_pair_dist = 0 
         self.avg_triplet_margin = 0
         self.record_these += ["avg_triplet_margin", "pos_pair_dist", "neg_pair_dist"]
+        self.type_of_triplets = type_of_triplets
 
     def mine(self, embeddings, labels):
         anchor_idx, positive_idx, negative_idx = lmu.get_all_triplets_indices(labels)
@@ -27,4 +34,8 @@ class TripletMarginMiner(BasePostGradientMiner):
         self.neg_pair_dist = torch.mean(an_dist).item()
         self.avg_triplet_margin = torch.mean(-triplet_margin).item()
         threshold_condition = triplet_margin > -self.margin
+        if self.type_of_triplets == "hard":
+        	threshold_condition &= an_dist < ap_dist
+        elif self.type_of_triplets == "semihard":
+        	threshold_condition &= an_dist > ap_dist
         return anchor_idx[threshold_condition], positive_idx[threshold_condition], negative_idx[threshold_condition]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytorch_metric_learning",
-    version="0.9.35",
+    version="0.9.36",
     author="Kevin Musgrave",
     author_email="tkm45@cornell.edu",
     description="A flexible and extensible metric learning library, written in PyTorch.",

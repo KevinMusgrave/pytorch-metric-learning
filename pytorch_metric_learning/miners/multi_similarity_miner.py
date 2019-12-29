51858cc75ea15859204e11aa9386a42b2f5db890
TakeshiMusgrave <tkm45@cornell.edu>
2019-12-29 06:15:15 -0500

    Vectorized the for-loop in MultiSimilarityMiner

--- pytorch_metric_learning/miners/multi_similarity_miner.py
+++ pytorch_metric_learning/miners/multi_similarity_miner.py
@@ -11,40 +11,29 @@ class MultiSimilarityMiner(BasePostGradientMiner):
         self.epsilon = epsilon
 
     def mine(self, embeddings, labels):
-        self.n = embeddings.size(0)
-        self.index_list = torch.arange(self.n).to(embeddings.device)
         self.sim_mat = lmu.sim_mat(embeddings)
-        return self.compute_indices(labels)
 
-    def compute_indices(self, labels):
-        empty_tensor = torch.tensor([]).long().to(labels.device)
-        a1_idx, p_idx, a2_idx, n_idx = [empty_tensor], [empty_tensor], [empty_tensor], [empty_tensor]
-        for i in range(self.n):
-            pos_indices = (
-                ((labels == labels[i]) & (self.index_list != i)).nonzero().flatten()
-            )
-            neg_indices = (labels != labels[i]).nonzero().flatten()
+        a1, p, a2, n = lmu.get_all_pairs_indices(labels)
 
-            if pos_indices.size(0) == 0 or neg_indices.size(0) == 0:
-                continue
+        if len(a1) == 0 or len(a2) == 0:
+            return [], [], [], []
 
-            pos_sorted, pos_sorted_idx = torch.sort(self.sim_mat[i][pos_indices])
-            neg_sorted, neg_sorted_idx = torch.sort(self.sim_mat[i][neg_indices])
-            neg_sorted_filtered_idx = (
-                (neg_sorted + self.epsilon > pos_sorted[0]).nonzero().flatten()
-            )
-            pos_sorted_filtered_idx = (
-                (pos_sorted - self.epsilon < neg_sorted[-1]).nonzero().flatten()
-            )
+        sim_mat_pos_sorting = self.sim_mat.clone()
+        sim_mat_neg_sorting = self.sim_mat.clone()
 
-            pos_indices = pos_indices[pos_sorted_idx][pos_sorted_filtered_idx]
-            neg_indices = neg_indices[neg_sorted_idx][neg_sorted_filtered_idx]
+        sim_mat_pos_sorting[a2, n] = float('inf')
+        sim_mat_neg_sorting[a1, p] = -float('inf')
+        sim_mat_neg_sorting[range(len(labels)), range(len(labels))] = -float('inf')
 
-            if len(pos_indices) > 0:
-                a1_idx.append(torch.ones_like(pos_indices) * i)
-                p_idx.append(pos_indices)
-            if len(neg_indices) > 0:
-                a2_idx.append(torch.ones_like(neg_indices) * i)
-                n_idx.append(neg_indices)
+        pos_sorted, pos_sorted_idx = torch.sort(sim_mat_pos_sorting, dim=1)
+        neg_sorted, neg_sorted_idx = torch.sort(sim_mat_neg_sorting, dim=1)
 
-        return [torch.cat(idx) for idx in [a1_idx, p_idx, a2_idx, n_idx]]
+        hard_pos_idx = (pos_sorted - self.epsilon < neg_sorted[:, -1].unsqueeze(1)).nonzero()
+        hard_neg_idx = (neg_sorted + self.epsilon > pos_sorted[:, 0].unsqueeze(1)).nonzero()
+
+        a1 = hard_pos_idx[:,0] 
+        p = pos_sorted_idx[a1, hard_pos_idx[:,1]]
+        a2 = hard_neg_idx[:,0]
+        n = neg_sorted_idx[a2, hard_neg_idx[:,1]]
+        
+        return a1, p, a2, n

2dd61bb34c3da8b13c6ce974a5a67cae0c788483
TakeshiMusgrave <tkm45@cornell.edu>
2020-05-14 15:05:54 -0400

    v0.9.86.dev3: Fix beta reg division bug in MarginLoss

--- src/pytorch_metric_learning/__init__.py
+++ src/pytorch_metric_learning/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.9.86.dev2"
+__version__ = "0.9.86.dev3"
--- src/pytorch_metric_learning/losses/margin_loss.py
+++ src/pytorch_metric_learning/losses/margin_loss.py
@@ -12,7 +12,7 @@ class MarginLoss(BaseMetricLossFunction):
         self.nu = nu
         self.beta = beta
         self.triplets_per_anchor = triplets_per_anchor
-        self.add_to_recordable_attributes(list_of_names=["num_pos_pairs", "num_neg_pairs"])
+        self.add_to_recordable_attributes(list_of_names=["num_pos_pairs", "num_neg_pairs", "margin_loss", "beta_reg_loss"])
         super().__init__(**kwargs)
 
     def compute_loss(self, embeddings, labels, indices_tuple):
@@ -23,7 +23,7 @@ class MarginLoss(BaseMetricLossFunction):
             return 0
         anchors, positives, negatives = embeddings[anchor_idx], embeddings[positive_idx], embeddings[negative_idx]
         beta = self.maybe_mask_param(self.beta, labels[anchor_idx])
-        beta_reg_loss = self.compute_reg_loss(beta)
+        self.beta_reg_loss = self.compute_reg_loss(beta)
 
         d_ap = torch.nn.functional.pairwise_distance(positives, anchors, p=2)
         d_an = torch.nn.functional.pairwise_distance(negatives, anchors, p=2)
@@ -36,10 +36,16 @@ class MarginLoss(BaseMetricLossFunction):
 
         pair_count = self.num_pos_pairs + self.num_neg_pairs 
 
-        return (torch.sum(pos_loss + neg_loss) + beta_reg_loss) / (pair_count + 1e-16)
+        if pair_count >= 1:
+            self.margin_loss = torch.sum(pos_loss + neg_loss) / pair_count
+            self.beta_reg_loss = self.beta_reg_loss / pair_count
+        else:
+            self.margin_loss, self.beta_reg_loss = 0, 0
+            
+        return self.margin_loss + self.beta_reg_loss
 
     def compute_reg_loss(self, beta):
         if self.nu > 0:
-            beta_mean = c_f.try_torch_operation(torch.mean, beta)
-            return beta_mean * self.nu
+            beta_sum = c_f.try_torch_operation(torch.sum, beta)
+            return beta_sum * self.nu
         return 0

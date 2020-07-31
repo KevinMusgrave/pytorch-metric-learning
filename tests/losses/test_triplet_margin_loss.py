061c41c913990342e556031f16137202d78e0f8d
TakeshiMusgrave <tkm45@cornell.edu>
2020-07-30 20:42:15 -0400

    BatchHardMiner works with float16, float32, float64

--- tests/losses/test_triplet_margin_loss.py
+++ tests/losses/test_triplet_margin_loss.py
@@ -5,42 +5,58 @@ from pytorch_metric_learning.utils import common_functions as c_f
 from pytorch_metric_learning.reducers import MeanReducer
 
 class TestTripletMarginLoss(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.device = torch.device('cuda')
+
     def test_triplet_margin_loss(self):
         margin = 0.2
         loss_funcA = TripletMarginLoss(margin=margin)
         loss_funcB = TripletMarginLoss(margin=margin, reducer=MeanReducer())
-        embedding_angles = [0, 20, 40, 60, 80]
-        embeddings = torch.tensor([c_f.angle_to_coord(a) for a in embedding_angles], requires_grad=True, dtype=torch.float) #2D embeddings
-        labels = torch.LongTensor([0, 0, 1, 1, 2])
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            embedding_angles = [0, 20, 40, 60, 80]
+            embeddings = torch.tensor([c_f.angle_to_coord(a) for a in embedding_angles], requires_grad=True, dtype=dtype).to(self.device) #2D embeddings
+            labels = torch.LongTensor([0, 0, 1, 1, 2])
 
-        lossA = loss_funcA(embeddings, labels)
-        lossB = loss_funcB(embeddings, labels)
-        lossA.backward()
-        lossB.backward()
-        
-        triplets = [(0,1,2), (0,1,3), (0,1,4), (1,0,2), (1,0,3), (1,0,4), (2,3,0), (2,3,1), (2,3,4), (3,2,0), (3,2,1), (3,2,4)]
+            lossA = loss_funcA(embeddings, labels)
+            lossB = loss_funcB(embeddings, labels)
+            
+            triplets = [(0,1,2), (0,1,3), (0,1,4), (1,0,2), (1,0,3), (1,0,4), (2,3,0), (2,3,1), (2,3,4), (3,2,0), (3,2,1), (3,2,4)]
 
-        correct_loss = 0
-        num_non_zero_triplets = 0
-        for a, p, n in triplets:
-            anchor, positive, negative = embeddings[a], embeddings[p], embeddings[n]
-            curr_loss = torch.relu(torch.sqrt(torch.sum((anchor-positive)**2)) - torch.sqrt(torch.sum((anchor-negative)**2)) + margin)
-            if curr_loss > 0:
-                num_non_zero_triplets += 1
-            correct_loss += curr_loss
-        self.assertTrue(torch.isclose(lossA, correct_loss/num_non_zero_triplets))
-        self.assertTrue(torch.isclose(lossB, correct_loss/len(triplets)))
+            correct_loss = 0
+            num_non_zero_triplets = 0
+            for a, p, n in triplets:
+                anchor, positive, negative = embeddings[a], embeddings[p], embeddings[n]
+                curr_loss = torch.relu(torch.sqrt(torch.sum((anchor-positive)**2)) - torch.sqrt(torch.sum((anchor-negative)**2)) + margin)
+                if curr_loss > 0:
+                    num_non_zero_triplets += 1
+                correct_loss += curr_loss
+            rtol = 1e-2 if dtype == torch.float16 else 1e-5
+            self.assertTrue(torch.isclose(lossA, correct_loss/num_non_zero_triplets, rtol=rtol))
+            self.assertTrue(torch.isclose(lossB, correct_loss/len(triplets), rtol=rtol))
 
 
     def test_with_no_valid_triplets(self):
         loss_funcA = TripletMarginLoss(margin=0.2)
         loss_funcB = TripletMarginLoss(margin=0.2, reducer=MeanReducer())
-        embedding_angles = [0, 20, 40, 60, 80]
-        embeddings = torch.tensor([c_f.angle_to_coord(a) for a in embedding_angles], requires_grad=True, dtype=torch.float) #2D embeddings
-        labels = torch.LongTensor([0, 1, 2, 3, 4])
-        lossA = loss_funcA(embeddings, labels)
-        lossA.backward()
-        lossB = loss_funcB(embeddings, labels)
-        lossB.backward()
-        self.assertEqual(lossA, 0)
-        self.assertEqual(lossB, 0)
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            embedding_angles = [0, 20, 40, 60, 80]
+            embeddings = torch.tensor([c_f.angle_to_coord(a) for a in embedding_angles], requires_grad=True, dtype=dtype).to(self.device) #2D embeddings
+            labels = torch.LongTensor([0, 1, 2, 3, 4])
+            lossA = loss_funcA(embeddings, labels)
+            lossB = loss_funcB(embeddings, labels)
+            self.assertEqual(lossA, 0)
+            self.assertEqual(lossB, 0)
+
+    def test_backward(self):
+        margin = 0.2
+        loss_funcA = TripletMarginLoss(margin=margin)
+        loss_funcB = TripletMarginLoss(margin=margin, reducer=MeanReducer())
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            for loss_func in [loss_funcA, loss_funcB]:
+                embedding_angles = [0, 20, 40, 60, 80]
+                embeddings = torch.tensor([c_f.angle_to_coord(a) for a in embedding_angles], requires_grad=True, dtype=dtype).to(self.device) #2D embeddings
+                labels = torch.LongTensor([0, 0, 1, 1, 2])
+
+                loss = loss_func(embeddings, labels)
+                loss.backward()
--- tests/miners/test_batch_hard_miner.py
+++ tests/miners/test_batch_hard_miner.py
@@ -8,49 +8,57 @@ class TestBatchHardMiner(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
+        self.device = torch.device('cuda')
         self.dist_miner = BatchHardMiner(use_similarity=False, normalize_embeddings=False)
         self.normalized_dist_miner = BatchHardMiner(use_similarity=False, normalize_embeddings=True)
         self.normalized_dist_miner_squared = BatchHardMiner(use_similarity=False, normalize_embeddings=True, squared_distances=True)
         self.sim_miner = BatchHardMiner(use_similarity=True, normalize_embeddings=True)
         self.labels = torch.LongTensor([0, 0, 1, 1, 0, 2, 1, 1, 1])
-        self.correct_a = torch.LongTensor([0, 1, 2, 3, 4, 6, 7, 8])
-        self.correct_p = torch.LongTensor([4, 4, 8, 8, 0, 2, 2, 2])
-        self.correct_n = [torch.LongTensor([2, 2, 1, 4, 3, 5, 5, 5]), torch.LongTensor([2, 2, 1, 4, 5, 5, 5, 5])]
+        self.correct_a = torch.LongTensor([0, 1, 2, 3, 4, 6, 7, 8]).to(self.device)
+        self.correct_p = torch.LongTensor([4, 4, 8, 8, 0, 2, 2, 2]).to(self.device)
+        self.correct_n = [torch.LongTensor([2, 2, 1, 4, 3, 5, 5, 5]).to(self.device), torch.LongTensor([2, 2, 1, 4, 5, 5, 5, 5]).to(self.device)]
 
     def test_dist_mining(self):
-        embeddings = torch.arange(9).float().unsqueeze(1)
-        a, p, n = self.dist_miner(embeddings, self.labels)
-        self.helper(a, p, n)
-        self.assertTrue(self.dist_miner.hardest_pos_pair_dist == 6)
-        self.assertTrue(self.dist_miner.hardest_neg_pair_dist == 1)
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            embeddings = torch.arange(9).type(dtype).unsqueeze(1).to(self.device)
+            a, p, n = self.dist_miner(embeddings, self.labels)
+            self.helper(a, p, n)
+            self.assertTrue(self.dist_miner.hardest_pos_pair_dist == 6)
+            self.assertTrue(self.dist_miner.hardest_neg_pair_dist == 1)
 
     def test_normalized_dist_mining(self):
-        angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
-        embeddings = torch.FloatTensor([c_f.angle_to_coord(a) for a in angles])
-        a, p, n = self.normalized_dist_miner(embeddings, self.labels)
-        self.helper(a, p, n)
-        correct_hardest_pos_pair_dist = torch.sqrt(torch.sum((embeddings[2]-embeddings[8])**2)).item()
-        correct_hardest_neg_pair_dist = torch.sqrt(torch.sum((embeddings[1]-embeddings[2])**2)).item()
-        self.assertAlmostEqual(self.normalized_dist_miner.hardest_pos_pair_dist, correct_hardest_pos_pair_dist, places=5)
-        self.assertAlmostEqual(self.normalized_dist_miner.hardest_neg_pair_dist, correct_hardest_neg_pair_dist, places=5)
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
+            embeddings = torch.tensor([c_f.angle_to_coord(a) for a in angles], dtype=dtype).to(self.device)
+            a, p, n = self.normalized_dist_miner(embeddings, self.labels)
+            self.helper(a, p, n)
+            correct_hardest_pos_pair_dist = torch.sqrt(torch.sum((embeddings[2]-embeddings[8])**2)).item()
+            correct_hardest_neg_pair_dist = torch.sqrt(torch.sum((embeddings[1]-embeddings[2])**2)).item()
+            places = 2 if dtype == torch.float16 else 5
+            self.assertAlmostEqual(self.normalized_dist_miner.hardest_pos_pair_dist, correct_hardest_pos_pair_dist, places=places)
+            self.assertAlmostEqual(self.normalized_dist_miner.hardest_neg_pair_dist, correct_hardest_neg_pair_dist, places=places)
 
     def test_normalized_dist_squared_mining(self):
-        angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
-        embeddings = torch.FloatTensor([c_f.angle_to_coord(a) for a in angles])
-        a, p, n = self.normalized_dist_miner_squared(embeddings, self.labels)
-        self.helper(a, p, n)
-        correct_hardest_pos_pair_dist = torch.sum((embeddings[2]-embeddings[8])**2).item()
-        correct_hardest_neg_pair_dist = torch.sum((embeddings[1]-embeddings[2])**2).item()
-        self.assertAlmostEqual(self.normalized_dist_miner_squared.hardest_pos_pair_dist, correct_hardest_pos_pair_dist, places=5)
-        self.assertAlmostEqual(self.normalized_dist_miner_squared.hardest_neg_pair_dist, correct_hardest_neg_pair_dist, places=5)            
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
+            embeddings = torch.tensor([c_f.angle_to_coord(a) for a in angles], dtype=dtype).to(self.device)
+            a, p, n = self.normalized_dist_miner_squared(embeddings, self.labels)
+            self.helper(a, p, n)
+            correct_hardest_pos_pair_dist = torch.sum((embeddings[2]-embeddings[8])**2).item()
+            correct_hardest_neg_pair_dist = torch.sum((embeddings[1]-embeddings[2])**2).item()
+            places = 2 if dtype == torch.float16 else 5
+            self.assertAlmostEqual(self.normalized_dist_miner_squared.hardest_pos_pair_dist, correct_hardest_pos_pair_dist, places=places)
+            self.assertAlmostEqual(self.normalized_dist_miner_squared.hardest_neg_pair_dist, correct_hardest_neg_pair_dist, places=places)            
 
     def test_sim_mining(self):
-        angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
-        embeddings = torch.FloatTensor([c_f.angle_to_coord(a) for a in angles])
-        a, p, n = self.sim_miner(embeddings, self.labels)
-        self.helper(a, p, n)
-        self.assertAlmostEqual(self.sim_miner.hardest_pos_pair_dist, np.cos(np.radians(120)), places=5)
-        self.assertAlmostEqual(self.sim_miner.hardest_neg_pair_dist, np.cos(np.radians(20)), places=5)
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            angles = [0, 20, 40, 60, 80, 100, 120, 140, 160]
+            embeddings = torch.tensor([c_f.angle_to_coord(a) for a in angles], dtype=dtype).to(self.device)
+            a, p, n = self.sim_miner(embeddings, self.labels)
+            self.helper(a, p, n)
+            places = 2 if dtype == torch.float16 else 5
+            self.assertAlmostEqual(self.sim_miner.hardest_pos_pair_dist, np.cos(np.radians(120)), places=places)
+            self.assertAlmostEqual(self.sim_miner.hardest_neg_pair_dist, np.cos(np.radians(20)), places=places)
 
     def helper(self, a, p, n):
         self.assertTrue(torch.equal(a, self.correct_a))
@@ -59,12 +67,13 @@ class TestBatchHardMiner(unittest.TestCase):
 
     def test_empty_output(self):
         batch_size = 32
-        embeddings = torch.randn(batch_size, 64)
-        labels = torch.arange(batch_size)
-        for miner in [self.dist_miner, self.normalized_dist_miner, self.normalized_dist_miner_squared, self.sim_miner]:
-            a, p, n = miner(embeddings, labels)
-            self.assertTrue(len(a)==0)
-            self.assertTrue(len(p)==0)
-            self.assertTrue(len(n)==0)
-            self.assertTrue(miner.hardest_pos_pair_dist == 0)
-            self.assertTrue(miner.hardest_neg_pair_dist == 0)
+        for dtype in [torch.float16, torch.float32, torch.float64]:
+            embeddings = torch.randn(batch_size, 64).type(dtype).to(self.device)
+            labels = torch.arange(batch_size)
+            for miner in [self.dist_miner, self.normalized_dist_miner, self.normalized_dist_miner_squared, self.sim_miner]:
+                a, p, n = miner(embeddings, labels)
+                self.assertTrue(len(a)==0)
+                self.assertTrue(len(p)==0)
+                self.assertTrue(len(n)==0)
+                self.assertTrue(miner.hardest_pos_pair_dist == 0)
+                self.assertTrue(miner.hardest_neg_pair_dist == 0)

4756c85f894ffb9d2dd1c41b121fb64b28b5bb90
TakeshiMusgrave <tkm45@cornell.edu>
2020-09-28 16:45:54 -0400

    Updated unit test workflow

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,13 @@ import sys
 import os
 
 sys.path.insert(0, "../pytorch-metric-learning/src")
+import pytorch_metric_learning
+
+print(
+    "testing pytorch_metric_learning version {} with pytorch version {}".format(
+        pytorch_metric_learning.__version__, torch.__version__
+    )
+)
 
 dtypes_from_environ = os.environ.get("TEST_DTYPES", "float16,float32,float64").split(
     ","

5af3a51992f4717827dece87a4a07a40f86a6ea4
TakeshiMusgrave <tkm45@cornell.edu>
2020-10-08 23:30:28 -0400

    v0.9.94.dev0: Fixed list mode bug in MultipleLosses. Allow list or dict or miners to be passed into MultipleLosses

--- src/pytorch_metric_learning/losses/base_metric_loss_function.py
+++ src/pytorch_metric_learning/losses/base_metric_loss_function.py
@@ -58,18 +58,25 @@ class BaseMetricLossFunction(
 
 
 class MultipleLosses(torch.nn.Module):
-    def __init__(self, losses, weights=None):
+    def __init__(self, losses, miners=None, weights=None):
         super().__init__()
         self.is_dict = isinstance(losses, dict)
         self.losses = (
             torch.nn.ModuleDict(losses) if self.is_dict else torch.nn.ModuleList(losses)
         )
-        if weights is not None:
-            assert (
-                isinstance(weights, dict)
+
+        if miners is not None:
+            self.assertions_if_not_none(miners, match_all_keys=False)
+            self.miners = (
+                torch.nn.ModuleDict(miners)
                 if self.is_dict
-                else c_f.is_list_or_tuple(weights)
+                else torch.nn.ModuleList(miners)
             )
+        else:
+            self.miners = None
+
+        if weights is not None:
+            self.assertions_if_not_none(weights, match_all_keys=True)
             self.weights = weights
         else:
             self.weights = (
@@ -79,11 +86,35 @@ class MultipleLosses(torch.nn.Module):
             )
 
     def forward(self, embeddings, labels, indices_tuple=None):
+        if self.miners:
+            assert indices_tuple is None
         total_loss = 0
         iterable = self.losses.items() if self.is_dict else enumerate(self.losses)
-        if self.is_dict:
-            for i, loss_func in iterable:
-                total_loss += (
-                    loss_func(embeddings, labels, indices_tuple) * self.weights[i]
-                )
+        for i, loss_func in iterable:
+            curr_indices_tuple = self.get_indices_tuple(
+                i, embeddings, labels, indices_tuple
+            )
+            total_loss += (
+                loss_func(embeddings, labels, curr_indices_tuple) * self.weights[i]
+            )
         return total_loss
+
+    def get_indices_tuple(self, i, embeddings, labels, indices_tuple):
+        if self.miners:
+            if (self.is_dict and i in self.miners) or (
+                not self.is_dict and self.miners[i] is not None
+            ):
+                indices_tuple = self.miners[i](embeddings, labels)
+        return indices_tuple
+
+    def assertions_if_not_none(self, x, match_all_keys):
+        if x is not None:
+            if self.is_dict:
+                assert isinstance(x, dict)
+                if match_all_keys:
+                    assert sorted(list(x.keys())) == sorted(list(self.losses.keys()))
+                else:
+                    assert all(k in self.losses.keys() for k in x.keys())
+            else:
+                assert c_f.is_list_or_tuple(x)
+                assert len(x) == len(self.losses)

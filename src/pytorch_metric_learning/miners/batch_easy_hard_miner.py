0bb30c6f65870175f7cf6ec59d80add34a3bd211
TakeshiMusgrave <tkm45@cornell.edu>
2020-12-10 22:36:51 -0500

    Added semihard and all options to BatchEasyHardMiner

--- src/pytorch_metric_learning/miners/batch_easy_hard_miner.py
+++ src/pytorch_metric_learning/miners/batch_easy_hard_miner.py
@@ -8,13 +8,15 @@ from ..utils import loss_and_miner_utils as lmu, common_functions as c_f
 
 class BatchEasyHardMiner(BaseTupleMiner):
     HARD = "hard"
+    SEMIHARD = "semihard"
     EASY = "easy"
-    all_batch_mining_strategies = [HARD, EASY]
+    ALL = "all"
+    all_batch_mining_strategies = [HARD, SEMIHARD, EASY, ALL]
 
     def __init__(
         self,
         pos_strategy=EASY,
-        neg_strategy=HARD,
+        neg_strategy=SEMIHARD,
         allowed_pos_range=None,
         allowed_neg_range=None,
         **kwargs
@@ -25,11 +27,18 @@ class BatchEasyHardMiner(BaseTupleMiner):
             pos_strategy in self.all_batch_mining_strategies
             and neg_strategy in self.all_batch_mining_strategies
         ):
-            raise NotImplementedError(
-                "\npos_strategy must be either \"{0}\"\nneg_strategy must be either \"{0}\"".format(
-                    "\" or \"".join(self.all_batch_mining_strategies)
+            raise ValueError(
+                '\npos_strategy must be one of "{0}"\nneg_strategy must be one of "{0}"'.format(
+                    '" or "'.join(self.all_batch_mining_strategies)
                 )
             )
+        if pos_strategy == neg_strategy == self.SEMIHARD:
+            raise ValueError('pos_strategy and neg_strategy cannot both be "semihard"')
+
+        if (pos_strategy == self.ALL and neg_strategy == self.SEMIHARD) or (
+            neg_strategy == self.ALL and pos_strategy == self.SEMIHARD
+        ):
+            raise ValueError('"semihard" cannot be used in combination with "all"')
 
         self.pos_strategy = pos_strategy
         self.neg_strategy = neg_strategy
@@ -38,9 +47,12 @@ class BatchEasyHardMiner(BaseTupleMiner):
 
         self.add_to_recordable_attributes(
             list_of_names=[
-                "triplet_dist",
-                "pos_pair_dist",
-                "neg_pair_dist",
+                "easiest_triplet",
+                "hardest_triplet",
+                "easiest_pos_pair",
+                "hardest_pos_pair",
+                "easiest_neg_pair",
+                "hardest_neg_pair",
             ],
             is_stat=True,
         )
@@ -48,27 +60,65 @@ class BatchEasyHardMiner(BaseTupleMiner):
     def mine(self, embeddings, labels, ref_emb, ref_labels):
         mat = self.distance(embeddings, ref_emb)
         a1_idx, p_idx, a2_idx, n_idx = lmu.get_all_pairs_indices(labels, ref_labels)
+        a = torch.arange(mat.size(0)).to(mat.device)
+
+        if self.pos_strategy == self.SEMIHARD and self.neg_strategy != self.ALL:
+            (negative_dists, negative_indices), a2n_keep = self.get_negatives(
+                mat, a2_idx, n_idx
+            )
+            (positive_dists, positive_indices), a1p_keep = self.get_positives(
+                mat, a1_idx, p_idx, negative_dists
+            )
+        elif self.neg_strategy == self.SEMIHARD and self.pos_strategy != self.ALL:
+            (positive_dists, positive_indices), a1p_keep = self.get_positives(
+                mat, a1_idx, p_idx
+            )
+            (negative_dists, negative_indices), a2n_keep = self.get_negatives(
+                mat, a2_idx, n_idx, positive_dists
+            )
+        else:
+            if self.pos_strategy != self.ALL:
+                (positive_dists, positive_indices), a1p_keep = self.get_positives(
+                    mat, a1_idx, p_idx
+                )
+            if self.neg_strategy != self.ALL:
+                (negative_dists, negative_indices), a2n_keep = self.get_negatives(
+                    mat, a2_idx, n_idx
+                )
+
+        if self.ALL not in [self.pos_strategy, self.neg_strategy]:
+            a_keep_idx = torch.where(a1p_keep & a2n_keep)
+            self.set_stats(positive_dists[a_keep_idx], negative_dists[a_keep_idx])
+            a = a[a_keep_idx]
+            p = positive_indices[a_keep_idx]
+            n = negative_indices[a_keep_idx]
+            return a, p, a, n
+        elif self.pos_strategy == self.ALL and self.neg_strategy != self.ALL:
+            self.set_stats(mat[a1_idx, p_idx], negative_dists[a2n_keep])
+            a2 = a[a2n_keep]
+            n = negative_indices[a2n_keep]
+            return a1_idx, p_idx, a2, n
+        elif self.pos_strategy != self.ALL and self.neg_strategy == self.ALL:
+            self.set_stats(positive_dists[a1p_keep], mat[a2_idx, n_idx])
+            a1 = a[a1p_keep]
+            p = positive_indices[a1p_keep]
+            return a1, p, a2_idx, n_idx
+        else:
+            self.set_stats(mat[a1_idx, p_idx], mat[a2_idx, n_idx])
+            return a1_idx, p_idx, a2_idx, n_idx
+
+    def get_positives(self, mat, a1_idx, p_idx, negative_dists=None):
         pos_func = self.get_mine_function(self.pos_strategy)
+        return pos_func(mat, a1_idx, p_idx, self.allowed_pos_range, negative_dists)
+
+    def get_negatives(self, mat, a2_idx, n_idx, positive_dists=None):
         neg_func = self.get_mine_function(
-            self.EASY if self.neg_strategy == self.HARD else self.HARD
+            self.EASY if self.neg_strategy in [self.HARD, self.SEMIHARD] else self.HARD
         )
-
-        (positive_dists, positive_indices), a1p_keep = pos_func(
-            mat, a1_idx, p_idx, self.allowed_pos_range
-        )
-        (negative_dists, negative_indices), a2n_keep = neg_func(
-            mat, a2_idx, n_idx, self.allowed_neg_range
-        )
-
-        a_keep_idx = torch.where(a1p_keep & a2n_keep)
-        self.set_stats(positive_dists[a_keep_idx], negative_dists[a_keep_idx])
-        a = torch.arange(mat.size(0)).to(positive_indices.device)[a_keep_idx]
-        p = positive_indices[a_keep_idx]
-        n = negative_indices[a_keep_idx]
-        return a, p, n
+        return neg_func(mat, a2_idx, n_idx, self.allowed_neg_range, positive_dists)
 
     def get_mine_function(self, strategy):
-        if strategy == self.HARD:
+        if strategy in [self.HARD, self.SEMIHARD]:
             mine_func = (
                 self.get_min_per_row
                 if self.distance.is_inverted
@@ -85,20 +135,28 @@ class BatchEasyHardMiner(BaseTupleMiner):
 
         return mine_func
 
-    def get_max_per_row(self, mat, anchor_idx, other_idx, val_range=None):
+    def get_max_per_row(
+        self, mat, anchor_idx, other_idx, val_range=None, semihard_thresholds=None
+    ):
         mask = torch.zeros_like(mat)
         mask[anchor_idx, other_idx] = 1
+        if semihard_thresholds is not None:
+            mask[mat >= semihard_thresholds.unsqueeze(1)] = 0
         if val_range is not None:
             mask[(mat > val_range[1]) | (mat < val_range[0])] = 0
         mat_masked = mat * mask
         non_zero_rows = torch.any(mask != 0, dim=1)
         return torch.max(mat_masked, dim=1), non_zero_rows
 
-    def get_min_per_row(self, mat, anchor_idx, other_idx, val_range=None):
+    def get_min_per_row(
+        self, mat, anchor_idx, other_idx, val_range=None, semihard_thresholds=None
+    ):
         pos_inf = c_f.pos_inf(mat.dtype)
         mask = torch.ones_like(mat) * pos_inf
         mask[anchor_idx, other_idx] = 1
 
+        if semihard_thresholds is not None:
+            mask[mat <= semihard_thresholds.unsqueeze(1)] = pos_inf
         if val_range is not None:
             mask[(mat > val_range[1]) | (mat < val_range[0])] = pos_inf
 
@@ -110,20 +168,31 @@ class BatchEasyHardMiner(BaseTupleMiner):
     def set_stats(self, positive_dists, negative_dists):
         if self.collect_stats:
             with torch.no_grad():
-                triplet_func = self.get_func_for_stats(True)
-                pos_func = self.get_func_for_stats(self.pos_strategy == self.HARD)
-                neg_func = self.get_func_for_stats(self.neg_strategy == self.EASY)
-
                 len_pd = len(positive_dists)
                 len_pn = len(negative_dists)
-                if len_pd > 0 and len_pn > 0:
-                    self.triplet_dist = triplet_func(
+                if (
+                    len_pd > 0
+                    and len_pn > 0
+                    and self.ALL not in [self.pos_strategy, self.neg_strategy]
+                ):
+                    easiest_triplet_func = self.get_func_for_stats(False)
+                    hardest_triplet_func = self.get_func_for_stats(True)
+                    self.easiest_triplet = easiest_triplet_func(
+                        positive_dists - negative_dists
+                    ).item()
+                    self.hardest_triplet = hardest_triplet_func(
                         positive_dists - negative_dists
                     ).item()
                 if len_pd > 0:
-                    self.pos_pair_dist = pos_func(positive_dists).item()
+                    easy_pos_func = self.get_func_for_stats(False)
+                    hard_pos_func = self.get_func_for_stats(True)
+                    self.easiest_pos_pair = easy_pos_func(positive_dists).item()
+                    self.hardest_pos_pair = hard_pos_func(positive_dists).item()
                 if len_pn > 0:
-                    self.neg_pair_dist = neg_func(negative_dists).item()
+                    easy_neg_func = self.get_func_for_stats(True)
+                    hard_neg_func = self.get_func_for_stats(False)
+                    self.easiest_neg_pair = easy_neg_func(negative_dists).item()
+                    self.hardest_neg_pair = hard_neg_func(negative_dists).item()
 
     def get_func_for_stats(self, min_if_inverted):
         if min_if_inverted:
--- src/pytorch_metric_learning/miners/batch_hard_miner.py
+++ src/pytorch_metric_learning/miners/batch_hard_miner.py
@@ -8,3 +8,7 @@ class BatchHardMiner(BatchEasyHardMiner):
             neg_strategy=BatchEasyHardMiner.HARD,
             **kwargs
         )
+
+    def mine(self, *args, **kwargs):
+        a1, p, a2, n = super().mine(*args, **kwargs)
+        return a1, p, n

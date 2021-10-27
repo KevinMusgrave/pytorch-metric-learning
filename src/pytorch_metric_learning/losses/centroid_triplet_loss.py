0d7c6184f69ac4dee5f8d59f55151d9383c85761
Chan Woo Kim <chanwkim01@gmail.com>
2021-10-27 09:03:13 +0000

    first working draft

--- src/pytorch_metric_learning/losses/centroid_triplet_loss.py
+++ src/pytorch_metric_learning/losses/centroid_triplet_loss.py
@@ -17,7 +17,7 @@ def concat_indices_tuple(x):
 class CentroidTripletLoss(BaseMetricLossFunction):
     def __init__(
         self,
-        allow_imbalanced=True,
+        allow_imbalanced=False,
         margin=0.05,
         swap=False,
         smooth_loss=False,
@@ -41,133 +41,96 @@ class CentroidTripletLoss(BaseMetricLossFunction):
         )
 
     def compute_loss(self, embeddings, labels, indices_tuple=None):
-        '''
-        "During training stage each mini-batch contains 𝑃 distinct item
-        classes with 𝑀 samples per class, resulting in batch size of 𝑃 × 𝑀."
-        '''
-        masks, labels_list, query_indices = self.create_masks_train(labels)
-        # print("MASKS", labels_list)
-        P = len(labels_list)
-        M = max([len(instances) for instances in labels_list])
-        DIM = embeddings.size(-1)
-
-        '''
-        "...each sample from S𝑘 is used as a query 𝑞𝑘 and the rest 
-        𝑀 −1 samples are used to build a prototype centroid"
-        i.e. for each class k of M items, we make M pairs of (query, centroid),
-        making a total of P*M total pairs.
-
-        masks = (M*P x len(embeddings)) matrix
-        labels_list[i] = indicies of embeddings belonging to ith class
-
-        centroids_emd.shape == (M*P, DIM)
-        i.e.    centroids_emb[0] == centroid vector for 0th class, where the first embedding is the query vector
-                centroids_emb[1] == centroid vector for 0th class, where the second embedding is the query vector
-                centroids_emb[M+1] == centroid vector for 1th class, where the first embedding is the query vector
-        '''
-      
+        masks, labels_list = self.create_masks_train(labels)  ## True for gallery
         masks = masks.to(embeddings.device)
-        masks_float = masks.float().to(embeddings.device)
-        inst_counts = masks_float.sum(-1)
-        valid_mask = inst_counts > 0
-        padded = masks_float.unsqueeze(-1) * embeddings.unsqueeze(0) 
-        centroids_emb = padded.sum(-2) / inst_counts.masked_fill(
-            inst_counts == 0, 1
+
+        masks_float = masks.type(embeddings.type()).to(embeddings.device)
+        padded = masks_float.unsqueeze(-1) * embeddings.unsqueeze(0)  # For broadcasting
+
+        M = len(masks)
+        P = len(labels_list)
+        D = len(embeddings[0])
+      
+        centroids_mask = masks.view(M, -1, M)
+        padded_tmp = padded.view(M, -1, M, D)
+      
+        valid_inst = centroids_mask.sum(-1)
+        valid_inst_bool = centroids_mask.sum(-1).bool()
+        centroids_emb = padded_tmp.sum(-2) / valid_inst.masked_fill(
+            valid_inst == 0, 1
         ).unsqueeze(-1)
 
-        query_indices = torch.tensor(query_indices).to(embeddings.device)
-        query_embeddings = embeddings.index_select(0, query_indices)
-        query_labels = labels.index_select(0, query_indices)
-        assert centroids_emb.size() == (M*P, DIM)
-        assert query_embeddings.size() == (M*P, DIM)
-
-        query_indices = query_indices.view((P, M)).transpose(0, 1)
-        query_embeddings = query_embeddings.view((P, M, -1)).transpose(0, 1)
-        query_labels = query_labels.view((P, M)).transpose(0, 1)
-        centroids_emb = centroids_emb.view((P, M, -1)).transpose(0, 1)
-        valid_mask = valid_mask.view((P, M)).transpose(0, 1)
-
-        combined_labels_collect = []
-        combined_embeddings_collect = []
+        embeddings_collect = []
+        labels_collect = []
         tuple_indices_collect = []
         starting_idx = 0
-        for inst_idx in range(M):
-            one_mask = valid_mask[inst_idx]
-            # print("one_mask", one_mask)
-            if torch.sum(one_mask) > 1:
-                one_queries = query_embeddings[inst_idx][one_mask]
-                one_centroids = centroids_emb[inst_idx][one_mask]
-                one_labels = query_labels[inst_idx][one_mask]
+        for i in range(M):
+            if valid_inst_bool[i].sum() <= 1:
+                continue
 
-                combined_embeddings = torch.cat((one_queries, one_centroids))
-                combined_labels = torch.cat((one_labels, one_labels))
-                
-                indices_tuple = lmu.get_all_triplets_indices(combined_labels)
-                # indices_tuple = c_f.shift_indices_tuple(indices_tuple, len(combined_labels))
-                indices_tuple = [x+starting_idx for x in indices_tuple]
-                tuple_indices_collect.append(indices_tuple)
+            current_mask = masks[i, :]
+            current_labels = labels[~current_mask]
+            query_feat = embeddings[~current_mask]
+            
+            current_centroids = centroids_emb[i]
+            current_centroids = current_centroids[
+                torch.abs(current_centroids).sum(1) > 1e-7
+            ]
 
-                starting_idx += len(combined_labels)
+            embeddings_concat = torch.cat((query_feat, current_centroids))
+            labels_concat = torch.cat((current_labels, current_labels))
 
-                combined_embeddings_collect.append(combined_embeddings)
-                combined_labels_collect.append(combined_labels)
+            indices_tuple = lmu.get_all_triplets_indices(labels_concat)
+            indices_tuple = [x[:len(x) // 2]+starting_idx for x in indices_tuple]
+            tuple_indices_collect.append(indices_tuple)
+            starting_idx += len(labels_concat)
 
-                # one_loss = self.triplet_loss(combined_embeddings, combined_labels)
+            embeddings_collect.append(embeddings_concat)
+            labels_collect.append(labels_concat)           
 
-                # loss_collect.append(one_loss)
         indices_tuple = concat_indices_tuple(tuple_indices_collect)
         if len(indices_tuple) == 0:
             return self.zero_losses()
-        final_embeddings = torch.cat(combined_embeddings_collect)
 
-        final_labels = torch.cat(combined_labels_collect)
+        final_embeddings = torch.cat(embeddings_collect)
+        final_labels = torch.cat(labels_collect)
 
 
         loss = self.triplet_loss.compute_loss(final_embeddings, final_labels, indices_tuple)
-        print("loss", loss)
-        return loss
 
+        return loss
 
     def create_masks_train(self, class_labels):
         labels_dict = defaultdict(list)
         class_labels = class_labels.detach().cpu().numpy()
         for idx, pid in enumerate(class_labels):
             labels_dict[pid].append(idx)
-
-        unique_classes = list(labels_dict.keys())
-        labels_list = list(labels_dict.values())
-
-        # labels_list = [v for k, v in labels_dict.items()]
+        labels_list = [v for k, v in labels_dict.items()]
         labels_list_copy = copy.deepcopy(labels_list)
         lens_list = [len(item) for item in labels_list]
         lens_list_cs = np.cumsum(lens_list)
 
-        M = max(len(instances) for instances in labels_list)  
-        P = len(unique_classes)
+        inst_per_class = [len(item) for item in labels_dict.values()]
+        
+        max_gal_num = max(inst_per_class)
+          ## TODO Should allow usage of all permuations
+        if False in [max_gal_num == one for one in inst_per_class] and not self.allow_imbalanced:
+            raise Exception("Unequal number of instances provided per label class. Set allow_imbalanced to True to surpress this warning.")
 
-        query_indices = []
-        masks = torch.zeros((M * P, len(class_labels)), dtype=bool)
-        for class_idx, class_insts in enumerate(labels_list):
-            for instance_idx in range(M):
-                matrix_idx = class_idx * M + instance_idx
-                if instance_idx < len(class_insts):
-                    query_indices.append(class_insts[instance_idx])
-                    ones = class_insts[:instance_idx] + class_insts[instance_idx+1:]
-                    masks[matrix_idx, ones] = 1
+
+        masks = torch.ones((max_gal_num, len(class_labels)), dtype=bool)
+        for _ in range(max_gal_num):
+            for i, inner_list in enumerate(labels_list):
+                if len(inner_list) > 0:
+                    # random.shuffle(inner_list)
+                    masks[_, inner_list.pop(0)] = 0
                 else:
-                    if self.allow_imbalanced:
-                        query_indices.append(0)
-                    else:
-                        raise Exception("Found uneven distribution of embeddings per label. "
-                        "Set allowed_imbalanced to True to surpress this error.")
-        return masks, labels_list, query_indices
+                    start_ind = lens_list_cs[i - 1]
+                    end_ind = start_ind + lens_list[i]
+                    masks[_, start_ind:end_ind] = 0
+
+        return masks, labels_list_copy
 
 
     def get_default_reducer(self):
         return AvgNonZeroReducer()
-
-    def get_default_distance(self):
-        return CosineSimilarity()
-
-    def _sub_loss_names(self):
-        return ["loss1", "loss2", "loss3"]
--- tests/losses/test_centroid_triplet_loss.py
+++ tests/losses/test_centroid_triplet_loss.py
@@ -2,6 +2,8 @@ import unittest
 
 import torch
 
+from itertools import chain
+
 from pytorch_metric_learning.distances import CosineSimilarity
 from pytorch_metric_learning.losses import CentroidTripletLoss
 from pytorch_metric_learning.reducers import MeanReducer
@@ -9,6 +11,8 @@ from pytorch_metric_learning.utils import common_functions as c_f
 
 from .. import TEST_DEVICE, TEST_DTYPES
 
+def normalize(embeddings):
+    return torch.nn.functional.normalize(embeddings, p=2, dim=0)
 
 class TestCentroidTripletLoss(unittest.TestCase):
     def test_centroid_triplet_loss(self):
@@ -22,66 +26,85 @@ class TestCentroidTripletLoss(unittest.TestCase):
         loss_funcE = CentroidTripletLoss(margin=margin, smooth_loss=True)
         
         for dtype in TEST_DTYPES:
-            embedding_angles = [0, 0, 40, 40, 80, 80]
+            per_class_angles = [
+                [0, 10, 20],
+                [30, 40, 50],
+            ]
+            centroid_makers = [
+                [[10, 20], [40, 50]],
+                [[0, 20], [30, 50]],
+                [[0, 10], [30, 40]]
+            ]
+            
+            embedding_angles = chain(*per_class_angles)
             embeddings = torch.tensor(
                 [c_f.angle_to_coord(a) for a in embedding_angles],
                 requires_grad=True,
                 dtype=dtype,
-            ).to(
-                TEST_DEVICE
-            )  # 2D embeddings
-            true_centroids = [
-                embeddings[(i*2): (i*2)+2].sum(-2) / 2
-                for i in range(len(embeddings) // 2)
-            ]
+            ).to(TEST_DEVICE)  # 2D embeddings
+            labels = torch.LongTensor([0, 0, 0, 1, 1, 1])
 
-            labels = torch.LongTensor([0, 0, 1, 1, 2, 2])
-
+            centroids = torch.tensor([
+                [
+                    [c_f.angle_to_coord(a) for a in coords] 
+                    for coords in one_maker]
+                for one_maker in centroid_makers
+            ], requires_grad=True, dtype=dtype).to(TEST_DEVICE)
+            centroids = centroids.sum(-2) / 2
+            
             [lossA, lossB, lossC, lossD, lossE] = [
                 x(embeddings, labels)
                 for x in [loss_funcA, loss_funcB, loss_funcC, loss_funcD, loss_funcE]
             ]
 
             triplets = [
-                (0, 1, 2),
-                (0, 1, 3),
-                (0, 1, 4),
-                (0, 1, 5),
+                (0, (0, 0), 3     ),
+                (0, (0, 0), (0, 1)),
+                (3, (0, 1), 0     ),
+                (3, (0, 1), (0, 0)),
 
-                (1, 0, 2),
-                (1, 0, 3),
-                (1, 0, 4),
-                (1, 0, 5),
-
-                (2, 3, 0),
-                (2, 3, 1),
-                (2, 3, 4),
-                (2, 3, 5),
-
-                (3, 2, 0),
-                (3, 2, 1),
-                (3, 2, 4),
-                (3, 2, 5)
+                (1, (1, 0), 4     ),
+                (1, (1, 0), (1, 1)),
+                (4, (1, 1), 1     ),
+                (4, (1, 1), (1, 0)),
+                
+                (2, (2, 0), 5    ),
+                (2, (2, 0), (2, 1)),
+                (5, (2, 1), 2    ),
+                (5, (2, 1), (2, 0)),
             ]
-            print("embeddings", embeddings)
-            print("true_centroids", true_centroids)
+
             correct_loss = 0
             correct_loss_cosine = 0
             correct_smooth_loss = 0
             num_non_zero_triplets = 0
             num_non_zero_triplets_cosine = 0
-            for a, p, n in triplets:
+            
+            cos_loss_col = []
+            for a, pc, nc in triplets:
                 anchor = embeddings[a]
-                positive = true_centroids[p // 2]
-                negative = true_centroids[n // 2]
+                if type(pc) == int:
+                    positive = embeddings[pc]
+                else:
+                    positive = centroids[pc[0]][pc[1]]
+
+                if type(nc) == int:
+                    negative = embeddings[nc]
+                else:
+                    negative = centroids[nc[0]][nc[1]]
 
                 ap_dist = torch.sqrt(torch.sum((anchor - positive) ** 2))
                 an_dist = torch.sqrt(torch.sum((anchor - negative) ** 2))
                 curr_loss = torch.relu(ap_dist - an_dist + margin)
                 
                 curr_loss_cosine = torch.relu(
-                    torch.sum(anchor * negative) - torch.sum(anchor * positive) + margin
+                    torch.sum(normalize(anchor) * normalize(negative)) - torch.sum(normalize(anchor) * normalize(positive)) + margin
                 )
+                
+                cos_loss_col.append(curr_loss_cosine)
+
+                ap_dist = torch.sqrt(torch.sum((normalize(anchor) - normalize(positive)) ** 2))
+                an_dist = torch.sqrt(torch.sum((normalize(anchor) - normalize(negative)) ** 2))
                 correct_smooth_loss += torch.nn.functional.softplus(
                     ap_dist - an_dist + margin
                 )
@@ -93,22 +116,18 @@ class TestCentroidTripletLoss(unittest.TestCase):
                 correct_loss_cosine += curr_loss_cosine
             rtol = 1e-2 if dtype == torch.float16 else 1e-5
 
-            print("lossA", lossA)
-            print("correct_loss", correct_loss)
-            print("num_non_zero_triplets", num_non_zero_triplets)
-            print("lossE", lossE)
-            print("correct_smooth_loss / len(triplets)", correct_smooth_loss / len(triplets))
-            # self.assertTrue(
-            #     torch.isclose(lossA, correct_loss / num_non_zero_triplets, rtol=rtol)
-            # )
+            
+            self.assertTrue(
+                torch.isclose(lossA, correct_loss / num_non_zero_triplets, rtol=rtol)
+            )
             self.assertTrue(
                 torch.isclose(lossB, correct_loss / len(triplets), rtol=rtol)
             )
-            # self.assertTrue(
-            #     torch.isclose(
-            #         lossC, correct_loss_cosine / num_non_zero_triplets_cosine, rtol=rtol
-            #     )
-            # )
+            self.assertTrue(
+                torch.isclose(
+                    lossC, correct_loss_cosine / num_non_zero_triplets_cosine, rtol=rtol
+                )
+            )
             self.assertTrue(
                 torch.isclose(lossD, correct_loss_cosine / len(triplets), rtol=rtol)
             )
@@ -116,7 +135,7 @@ class TestCentroidTripletLoss(unittest.TestCase):
                 torch.isclose(lossE, correct_smooth_loss / len(triplets), rtol=rtol)
             )
 
-    def test_with_no_valid_triplets_no_imbalance(self):
+    def test_no_allow_imbalanced(self):
         loss_funcA = CentroidTripletLoss(margin=0.2)
         loss_funcB = CentroidTripletLoss(margin=0.2, reducer=MeanReducer())
         for dtype in TEST_DTYPES:
@@ -128,13 +147,12 @@ class TestCentroidTripletLoss(unittest.TestCase):
             ).to(
                 TEST_DEVICE
             )  # 2D embeddings
-            labels = torch.LongTensor([0, 1, 2, 3, 4])
+            labels = torch.LongTensor([0, 1, 1, 2, 2])
 
-            lossA = loss_funcA(embeddings, labels)
-            lossB = loss_funcB(embeddings, labels)
-            self.assertEqual(lossA, 0)
-            self.assertEqual(lossB, 0)
-
+            with self.assertRaises(Exception):
+                lossA = loss_funcA(embeddings, labels)
+                lossB = loss_funcB(embeddings, labels)
+    
     def test_backward(self):
         margin = 0.2
         loss_funcA = CentroidTripletLoss(margin=margin)
@@ -142,7 +160,7 @@ class TestCentroidTripletLoss(unittest.TestCase):
         loss_funcC = CentroidTripletLoss(smooth_loss=True)
         for dtype in TEST_DTYPES:
             for loss_func in [loss_funcA, loss_funcB, loss_funcC]:
-                embedding_angles = [0, 20, 40, 60, 80]
+                embedding_angles = [0, 20, 40, 60, 80, 85]
                 embeddings = torch.tensor(
                     [c_f.angle_to_coord(a) for a in embedding_angles],
                     requires_grad=True,
@@ -150,7 +168,7 @@ class TestCentroidTripletLoss(unittest.TestCase):
                 ).to(
                     TEST_DEVICE
                 )  # 2D embeddings
-                labels = torch.LongTensor([0, 0, 1, 1, 2])
+                labels = torch.LongTensor([0, 0, 1, 1, 2, 2])
 
                 loss = loss_func(embeddings, labels)
                 loss.backward()

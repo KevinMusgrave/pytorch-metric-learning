fef964d51a278fac7da15f51a3d82dc6e1c56473
TakeshiMusgrave <tkm45@cornell.edu>
2022-02-10 02:27:50 -0500

    Undo temp change to build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
-# ./format_code.sh
-# python -m unittest discover && \
+./format_code.sh
+python -m unittest discover && \
 rm -rfv build/ && \
 rm -rfv dist/ && \
 rm -rfv src/pytorch_metric_learning.egg-info/ && \

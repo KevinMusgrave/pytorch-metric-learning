4147ee7380455cef7ea390b5860f9a3c6cd2e185
KevinMusgrave <tkm45@cornell.edu>
2022-06-09 12:48:30 -0400

    Test against original implementation

--- src/pytorch_metric_learning/losses/instance_loss.py
+++ src/pytorch_metric_learning/losses/instance_loss.py
@@ -2,22 +2,18 @@ import torch
 import torch.nn.functional as F
 
 from ..distances import CosineSimilarity
-from ..reducers import AvgNonZeroReducer
+from ..reducers import MeanReducer
 from ..utils import common_functions as c_f
-from ..utils import loss_and_miner_utils as lmu
 from .base_metric_loss_function import BaseMetricLossFunction
 
-def l2_norm(v):
-    fnorm = torch.norm(v, p=2, dim=1, keepdim=True) + 1e-6
-    v = v.div(fnorm.expand_as(v))
-    return v
 
 class InstanceLoss(BaseMetricLossFunction):
     """
-    Implementation of  Dual-Path Convolutional Image-Text Embeddings with Instance Loss, ACM TOMM 2020 
-    https://arxiv.org/abs/1711.05535 
+    Implementation of  Dual-Path Convolutional Image-Text Embeddings with Instance Loss, ACM TOMM 2020
+    https://arxiv.org/abs/1711.05535
     using cross-entropy loss for every sample if label is not available, else use given label.
     """
+
     def __init__(self, gamma=64, **kwargs):
         super().__init__(**kwargs)
         c_f.assert_distance_type(self, CosineSimilarity)
@@ -28,10 +24,11 @@ class InstanceLoss(BaseMetricLossFunction):
         )
 
     def compute_loss(self, embeddings, labels, indices_tuple, ref_emb, ref_labels):
-        normed_feature = l2_norm(embeddings)
-        sim1 = torch.mm(normed_feature*self.gamma, torch.t(normed_feature)) 
+        c_f.indices_tuple_not_supported(indices_tuple)
+        c_f.ref_not_supported(embeddings, labels, ref_emb, ref_labels)
+        sim1 = self.distance(embeddings) * self.gamma
         if labels is None:
-            sim_label = torch.arange(sim1.size(0)).cuda().detach()
+            sim_label = c_f.torch_arange_from_size(embeddings)
         else:
             _, sim_label = torch.unique(labels, return_inverse=True)
         loss = F.cross_entropy(sim1, sim_label, reduction="none")
@@ -44,7 +41,7 @@ class InstanceLoss(BaseMetricLossFunction):
         }
 
     def get_default_reducer(self):
-        return AvgNonZeroReducer()
+        return MeanReducer()
 
     def get_default_distance(self):
         return CosineSimilarity()
--- tests/losses/test_instance_loss.py
+++ tests/losses/test_instance_loss.py
@@ -1,36 +1,64 @@
 import unittest
 
+import numpy as np
 import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
 
 from pytorch_metric_learning.losses import InstanceLoss
 
 from .. import TEST_DEVICE, TEST_DTYPES
-from ..zzz_testing_utils.testing_utils import angle_to_coord
 
+
+# original loss copied from here
+# https://github.com/layumi/Person_reID_baseline_pytorch/blob/master/instance_loss.py
+# as a reference to test against
+def l2_norm(v):
+    fnorm = torch.norm(v, p=2, dim=1, keepdim=True) + 1e-6
+    v = v.div(fnorm.expand_as(v))
+    return v
+
+
+class OriginalInstanceLoss(nn.Module):
+    def __init__(self, gamma=1) -> None:
+        super().__init__()
+        self.gamma = gamma
+
+    def forward(self, feature, label=None) -> Tensor:
+        # Dual-Path Convolutional Image-Text Embeddings with Instance Loss, ACM TOMM 2020
+        # https://zdzheng.xyz/files/TOMM20.pdf
+        # using cross-entropy loss for every sample if label is not available. else use given label.
+        normed_feature = l2_norm(feature)
+        sim1 = torch.mm(normed_feature * self.gamma, torch.t(normed_feature))
+        # sim2 = sim1.t()
+        if label is None:
+            sim_label = torch.arange(sim1.size(0)).cuda().detach()
+        else:
+            _, sim_label = torch.unique(label, return_inverse=True)
+        loss = F.cross_entropy(sim1, sim_label)  # + F.cross_entropy(sim2, sim_label)
+        return loss
 
 
 class TestInstanceLoss(unittest.TestCase):
     def test_instance_loss(self):
+        torch.manual_seed(3029)
         for dtype in TEST_DTYPES:
-            la = 1 if dtype == torch.float16 else 20
-            gamma = 1 if dtype == torch.float16 else 0.1
             for gamma in range(1, 256, 32):
-                loss_func = InstanceLoss(
-                    gamma=gamma,
-                ).to(TEST_DEVICE)
-
-                embedding_angles = torch.arange(0, 180)
-                embeddings = torch.tensor(
-                    [angle_to_coord(a) for a in embedding_angles],
-                    requires_grad=True,
-                    dtype=dtype,
-                ).to(
-                    TEST_DEVICE
-                )  # 2D embeddings
-                labels = torch.randint(low=0, high=10, size=(180,)).to(TEST_DEVICE)
-                
-                loss = loss_func(embeddings, labels)
+                loss_fn = InstanceLoss(gamma=gamma)
+                original_loss_fn = OriginalInstanceLoss(gamma=gamma)
+                embeddings = torch.randn(
+                    32, 128, device=TEST_DEVICE, dtype=dtype, requires_grad=True
+                )
+                labels = torch.randint(0, 10, size=(32,), device=TEST_DEVICE)
+                loss = loss_fn(embeddings, labels)
                 loss.backward()
+
+                original_loss = original_loss_fn(embeddings, labels)
+                rtol = 1e-3 if dtype == torch.float16 else 1e-6
+                self.assertTrue(
+                    np.isclose(loss.item(), original_loss.item(), rtol=rtol)
+                )
+
 
 if __name__ == "__main__":
     unittest.main()

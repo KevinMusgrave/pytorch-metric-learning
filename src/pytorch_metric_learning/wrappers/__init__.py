877cb59d1b0b8691e16cccce110d630caed7189a
KevinMusgrave <tkm45@cornell.edu>
2023-01-29 15:06:22 -0500

    Remove the wrappers folder

--- src/pytorch_metric_learning/wrappers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_loss_wrapper import BaseLossWrapper
-from .cross_batch_memory import CrossBatchMemory
-from .multiple_losses import MultipleLosses
-from .self_supervised_loss import SelfSupervisedLoss

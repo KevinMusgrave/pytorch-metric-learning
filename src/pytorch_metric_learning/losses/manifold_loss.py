bug fix. There is a problem of numerical cancellation in the original manifold loss.

src/pytorch_metric_learning/losses/manifold_loss.py
@@ -1,73 +1,42 @@
+# /usr/bin/env python3
 import torch
-from pytorch_metric_learning.distances import DotProductSimilarity, CosineSimilarity
+from torch import nn
+from pytorch_metric_learning.distances import CosineSimilarity
 from pytorch_metric_learning.utils import common_functions as c_f
 from pytorch_metric_learning.losses.base_metric_loss_function import BaseMetricLossFunction
-from torch_scatter import scatter_add
-
-
-class ManifoldProxyLoss(BaseMetricLossFunction):
-    def __init__(self, p: torch.tensor, **kwargs):
-        super().__init__(**kwargs)
-        self.p = p
-
-    def compute_loss(self, embeddings, labels, indices_tuple, ref_emb, ref_labels):
-        c_f.labels_not_supported(labels, ref_labels)
-        ref_emb, indices_tuple, self.p = c_f.to_device((ref_emb, indices_tuple, self.p), tensor=embeddings,
-                                                       dtype=torch.float32)
-        indices_tuple = indices_tuple.to(torch.long)
-
-        K = ref_emb.shape[0]
-        proxies_by_embeddings = ref_emb[indices_tuple, :]
-        p_by_embeddings = self.p[indices_tuple, :]
-        loss = torch.exp(self.distance(p_by_embeddings, embeddings).diag() -
-                         self.distance(p_by_embeddings, proxies_by_embeddings).diag())
-        loss = scatter_add(loss, indices_tuple, dim_size=K).view(K, 1)
-        loss -= torch.exp(self.distance(self.p, ref_emb).diag() - 1).view(-1,1)
-        loss = torch.log(1 + loss)
-
-        return {
-            "loss": {
-                "losses": loss,
-                "indices": c_f.torch_arange_from_size(ref_emb),
-                "reduction_type": "element",
-            }
-        }
-
-    def get_default_distance(self):
-        return CosineSimilarity()
 
 
 class ManifoldLoss(BaseMetricLossFunction):
     r"""
     The parameters are defined as in the paper https://openaccess.thecvf.com/content_CVPR_2019/papers/Aziere_Ensemble_Deep_Manifold_Similarity_Learning_Using_Hard_Proxies_CVPR_2019_paper.pdf
-    K: number of proxies.
-
     - l: embedding size.
 
-    - method: type of manifold loss to compute. Available choices are ['intrinsic', 'context'].
+    - K: number of proxies. Optional
+
+    - lambdaC: regularization weight. Used in the formula
+                :math:`loss = loss^{int} + \lambda_C*loss^{ctx}`.
+        For :math:`\lambda_C=0` use only intrinsic loss, for :math:`\lambda_C=\infty` use only context loss.
+        Optional
 
     - alpha: parameter of the Random Walk. It is contained in :math:`(0,1)` and specifies the amount of similarity from
-           each node is passed to neighbor nodes.
+           each node is passed to neighbor nodes. Optional
 
-    - margin: margin used in the calculation of the loss.
+    - margin: margin used in the calculation of the loss. Optional
     """
 
-    def __init__(self, K=50, l=128, method="intrinsic", alpha=0.8, margin=5e-4, **kwargs):
+    def __init__(self, l: int, K: int = 50, lambdaC: float = 1.0, alpha: float = 0.8, margin: float = 5e-4, **kwargs):
         super().__init__(**kwargs)
-        if 'intrinsic' not in method and 'context' not in method:
-            raise TypeError(f"Method {method} not supported. Available choices are ['intrinsic', 'context'].")
+        if lambdaC < 0:
+            raise ValueError(f"Uncorrect value for lambdaC argument. "
+                             f"Given lambdaC={lambdaC} but accepted only non-negative values")
 
         self.K = K
         self.l = l
-        self.proxies = torch.rand(K, l, requires_grad=True)
-        self.method = method
+        self.proxies = nn.Parameter(torch.randn(K, l))
+        self.lambdaC = lambdaC
         self.alpha = alpha
         self.margin = margin
-
-        self.p = torch.randn(K, l, requires_grad=True)
-        self.proxies_loss = self.get_default_proxy_loss()
-        self.proxies_optimizer = self.get_default_optimizer()
-        self.add_to_recordable_attributes(list_of_names=["K", "l", "method", "alpha", "margin"], is_stat=True)
+        self.add_to_recordable_attributes(list_of_names=["K", "l", "lambdaC", "alpha", "margin"], is_stat=True)
 
     def compute_loss(self, embeddings, labels, indices_tuple, ref_emb, ref_labels):
         c_f.ref_not_supported(embeddings, labels, ref_emb, ref_labels)
@@ -86,38 +55,50 @@ class ManifoldLoss(BaseMetricLossFunction):
         if torch.all(self.proxies == 0):
             self.proxies = self.get_default_proxies(embeddings, meta_classes)
 
-        self.proxies = c_f.to_device(self.proxies, tensor=embeddings)
+        loss_int = torch.zeros(1)
+        loss_int = c_f.to_device(loss_int, tensor=embeddings, dtype=embeddings.dtype)
         embs_and_proxies = torch.cat([embeddings, self.proxies], dim=0)
-        S = self.distance(embs_and_proxies, embs_and_proxies)
-        D_inv_half = torch.pow(torch.abs(torch.sum(S, dim=1)),
-                               -1 / 2)  # In the paper it is not specified how to avoid averall negative scalar products
-        S_bar = D_inv_half * S.t()
-        S_bar = S_bar.t() * D_inv_half
-        S_bar.fill_diagonal_(0)
-        F = (1 - self.alpha) * torch.inverse(torch.eye(N + self.K, N + self.K, device=S_bar.device)
-                                             - self.alpha * S_bar)[:, -self.K:]
 
-        if 'intrinsic' in self.method:
-            F = F[:N, :]
-            loss = torch.exp(F - F[torch.arange(N), meta_classes].view(N, 1) + self.margin)
-            loss = torch.log(1 + torch.sum(loss, dim=1))
-            loss = loss.mean()
-        else:
-            proxies_repeated = F[-self.K:, :].unsqueeze(0).repeat(N, 1, 1)
-            loss = torch.exp(ManifoldLoss.s(F[:N, :].unsqueeze(-1), proxies_repeated) -
-                             ManifoldLoss.s(F[:N, :], F[N + meta_classes, :]).unsqueeze(-1) + self.margin)
-            loss = torch.log(1 + torch.sum(loss, dim=1))
-            loss = loss.mean()
+        S = self.distance(embs_and_proxies, embs_and_proxies).clamp(0, torch.inf)
+        S = torch.exp(S / 0.5)
+        Y = torch.eye(N + self.K, device=S.device, dtype=S.dtype)
+        S.fill_diagonal_(0)
 
-        old_proxies = self.proxies.detach()
-        old_embs = embeddings.detach()
+        D_inv_half = torch.pow(torch.sum(S, dim=1), -1 / 2).diag()
+        S_bar = D_inv_half.mm(S)
+        S_bar = S_bar.mm(D_inv_half)
 
-        for _ in range(20):
-            proxies_loss = self.proxies_loss(old_embs, None, meta_classes, old_proxies, None)
-            self.proxies_optimizer.zero_grad()
-            proxies_loss.backward()
-            self.proxies_optimizer.step()
-        self.proxies = self.p
+        dt = S_bar.dtype
+        L = torch.inverse(Y.float() - self.alpha * S_bar.float())  # Added dtype cast to avoid errors
+        L = L.to(dt)
+
+        F = (1 - self.alpha) * L
+
+        F_p = F[N:, :].clone()
+        F_e = F[:N, :].clone()
+        if self.lambdaC != torch.inf:
+            F = F[:N, N:]
+            loss_int = F - F[torch.arange(N), meta_classes].view(-1, 1) + self.margin
+            loss_int[
+                torch.arange(N), meta_classes] = -torch.inf  # This way avoid numerical cancellation happening   # NoQA
+            # instead with subtraction of margin term           # NoQA
+            loss_int[loss_int < 0] = -torch.inf  # This way no loss for positive correlation with own proxy
+
+            loss_int = torch.exp(loss_int)
+            loss_int = torch.log(1 + torch.sum(loss_int, dim=1))
+            loss_int = loss_int.mean()
+
+        loss_ctx = torch.nn.functional.cosine_similarity(F_e, F_p.unsqueeze(1), dim=-1).t()
+        loss_ctx += -loss_ctx[torch.arange(N), meta_classes].view(-1, 1) + self.margin
+        loss_ctx[torch.arange(N), meta_classes] = -torch.inf  # This way avoid numerical cancellation happening   # NoQA
+        # instead with subtraction of margin term           # NoQA
+        loss_ctx[loss_ctx < 0] = -torch.inf
+
+        # This way no loss for positive correlation with own proxy
+        loss_ctx = torch.exp(loss_ctx)
+        loss_ctx = torch.log(1 + torch.sum(loss_ctx, dim=1))
+        loss_ctx = loss_ctx.mean()
+        loss = loss_int + self.lambdaC * loss_ctx
 
         return {
             "loss": {
@@ -127,12 +108,18 @@ class ManifoldLoss(BaseMetricLossFunction):
             }
         }
 
-    @staticmethod
-    def s(x, p):
-        return torch.sum(x * p, dim=1)
+    # @staticmethod
+    # def s(x, p):
+    #     return torch.sum(x / x.norm(p=2, dim=1, keepdim=True) * p / p.norm(p=2, dim=1, keepdim=True), dim=1)
+
+    def update_p(self, old_embs, old_proxies, meta_classes):
+        proxies_loss = self.proxies_loss(old_embs, None, meta_classes, old_proxies, None)
+        self.proxies_optimizer.zero_grad()
+        proxies_loss.backward()
+        self.proxies_optimizer.step()
 
     def get_default_distance(self):
-        return DotProductSimilarity()
+        return CosineSimilarity()
 
     def get_default_proxies(self, embs, meta_classes):
         self.proxies = []
@@ -140,25 +127,24 @@ class ManifoldLoss(BaseMetricLossFunction):
             meta_class_k = embs[meta_classes == k, :]
             meta_class_k_row_index = torch.randint(meta_class_k.shape[0], (1,))
             self.proxies.append(meta_class_k[meta_class_k_row_index, :])
-        return torch.cat(self.proxies, dim=0)
+        return torch.cat(self.proxies, dim=0).to(device=embs.device)
 
     def get_default_optimizer(self):
         return torch.optim.SGD([self.p], lr=0.01, momentum=0.9)
 
-    def get_default_proxy_loss(self):
-        return ManifoldProxyLoss(self.p)
+    # def get_default_proxy_loss(self):
+    #     return ManifoldProxyLoss(self.p)
 
-
-if __name__ == '__main__':
-    loss_fn = ManifoldLoss(K=3)
-    for _ in range(10, 20):
-        data = torch.stack([
-            torch.log(torch.arange(_, 128 + _)),
-            torch.cos(torch.arange(_, 128 + _)),
-            torch.exp(torch.linspace(0, 1 / _, 128)),
-            torch.arange(_, 128 + _).pow(2),
-            5 * torch.arange(_, 128 + _).pow(1),
-            torch.arange(_, 128 + _).pow(3) - 3 * torch.arange(_, 128 + _).pow(1.5)
-        ])
-        loss = loss_fn(data, None, None, None, None)
-        print(loss)
+# if __name__ == '__main__':
+#     loss_fn = ManifoldLoss(l=128, K=3)
+#     for _ in range(10, 20):
+#         data = torch.stack([
+#             torch.log(torch.arange(_, 128 + _)),
+#             torch.cos(torch.arange(_, 128 + _)),
+#             torch.exp(torch.linspace(0, 1 / _, 128)),
+#             torch.arange(_, 128 + _).pow(2),
+#             5 * torch.arange(_, 128 + _).pow(1),
+#             torch.arange(_, 128 + _).pow(3) - 3 * torch.arange(_, 128 + _).pow(1.5)
+#         ])
+#         loss = loss_fn(data, None, None, None, None)
+#         print(loss)

src/pytorch_metric_learning/losses/p2s_grad_loss.py
@@ -59,7 +59,7 @@ class P2SGradLoss(BaseMetricLossFunction):
 
         with torch.no_grad():
             index = torch.zeros_like(cos_theta)
-            index.scatter_(1, labels.data.view(-1, 1), 1)
+            index.scatter_(1, labels.view(-1, 1), 1)
 
         # MSE between \cos\theta and one-hot vectors
         loss = self.m_loss(cos_theta, index)

src/pytorch_metric_learning/utils/common_functions.py
@@ -3,11 +3,12 @@ import glob
 import logging
 import os
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import numpy as np
 import scipy.stats
 import torch
+from torch import nn
 
 LOGGER_NAME = "PML"
 LOGGER = logging.getLogger(LOGGER_NAME)
@@ -460,8 +461,8 @@ def to_dtype(x, tensor=None, dtype=None):
     return x
 
 
-def to_device(x: torch.Tensor | List[torch.Tensor] | Tuple[torch.Tensor], tensor=None, device=None,
-              dtype: torch.Tensor | List[torch.Tensor] | Tuple[torch.Tensor] = None):
+def to_device(x: Union[torch.Tensor, nn.Parameter], tensor=None, device=None,
+              dtype: Union[torch.Tensor, List[torch.Tensor], Tuple[torch.Tensor]] = None):
     dv = device if device is not None else tensor.device
     is_iterable = is_list_or_tuple(x)
     if not is_iterable:
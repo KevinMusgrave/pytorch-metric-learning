892f8f724decf3d3576c97864c47d56ef36d3935
KevinMusgrave <tkm45@cornell.edu>
2024-11-02 20:29:29 +0000

    minor change to tests

--- tests/utils/test_inference.py
+++ tests/utils/test_inference.py
@@ -39,7 +39,7 @@ class TestInference(unittest.TestCase):
 
         transform = transforms.Compose(
             [
-                transforms.Resize(64),
+                transforms.Resize(256),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]

206e8396c72113b0137e101500444c2ac8f72ceb
KevinMusgrave <tkm45@cornell.edu>
2024-11-02 21:41:12 +0000

    Revert "minor change to tests"
    This reverts commit 892f8f724decf3d3576c97864c47d56ef36d3935.

--- tests/samplers/test_tuples_to_weights_sampler.py
+++ tests/samplers/test_tuples_to_weights_sampler.py
@@ -22,7 +22,7 @@ class TestTuplesToWeightsSampler(unittest.TestCase):
 
         eval_transform = transforms.Compose(
             [
-                transforms.Resize(256),
+                transforms.Resize(128),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
